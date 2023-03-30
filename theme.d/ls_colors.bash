1190061e91b84e955dc2d308b41a3cd350a2fd6f
kotfu <kotfu@kotfu.net>
2023-03-30 16:33:17 -0600

    Clean up a bunch of old cruft from when this was all done with scripts

--- theme.d/ls_colors.bash
+++
@@ -1,7 +0,0 @@
-#
-# gotta run this like ". ls_colors" or it won't work
-
-# set the ls colors from the theme
-if [[ -f "$THEME_DIR/ls_colors.bash" ]]; then
-    source "$THEME_DIR/ls_colors.bash"
-fi
